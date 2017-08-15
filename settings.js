9e22fa7883f6aed2b7e788d3fc00448b8ac0722b
apschaut1 <aaron@theimageshoppe.com>
2017-08-15 12:41:41 -0400

    template

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ exports.listId = 1558826;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = "8921ff2c-d365-4ee6-9e80-94d6de3242de";
+exports.templateId = "34652f4d-dee8-462c-a843-0dcd7ac05e42";
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
