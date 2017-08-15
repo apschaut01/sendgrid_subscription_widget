6ed49814ccc3f433b31ea2b24642f65d56fea656
apschaut1 <aaron@theimageshoppe.com>
2017-08-15 14:59:08 -0400

    update string on id

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ exports.senderName = "Essence Restaurants Group";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = 1558826;
+exports.listId = "1558826";
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
