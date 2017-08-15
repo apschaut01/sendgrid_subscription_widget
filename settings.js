5819aaffc25b70ad6693ac3f22ea5ae9dd2ec4f8
apschaut1 <aaron@theimageshoppe.com>
2017-08-15 12:22:05 -0400

    test

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ exports.senderName = "Essence Restaurants Group";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = "1558826";
+exports.listId = 1558826;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = "8921ff2c-d365-4ee6-9e80-94d6de3242de";
+exports.templateId = 8921ff2c-d365-4ee6-9e80-94d6de3242de;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
